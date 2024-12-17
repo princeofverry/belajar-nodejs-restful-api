09c901991f80b60e5168182d9718644b6ca21af5
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-17 10:51:30 +0700

    public-api for routes

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,4 +1,15 @@
-const register = (req, res) => {};
+import userService from "../service/user-service";
+
+const register = (req, res, next) => {
+  try {
+    const result = userService.register(req.body);
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
 
 export default {
   register,
---
+++ src/route/public-api.js
@@ -0,0 +1,7 @@
+import express from "express";
+import userController from "../controller/user-controller";
+
+const publicRouter = express.Router();
+publicRouter.post("/api/users", userController.register);
+
+export { publicRouter };
