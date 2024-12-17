53d2f14a75dd26180edbaef1308b18b3b1445149
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-17 11:57:19 +0700

    fix: dulu

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const registerUserValidation = Joi.object({
-  username: Joi.string().max(100).required,
+  username: Joi.string().max(100).required(),
   password: Joi.string().max(100).required(),
   name: Joi.string().max(100).required(),
 });
---
+++ test/user.test.js
@@ -0,0 +1,30 @@
+import supertest from "supertest";
+import { web } from "../src/application/web.js";
+import { prismaClient } from "../src/application/database.js";
+
+describe("POST /api/users", function () {
+  beforeEach(async () => {
+    await prismaClient.user.deleteMany();
+  });
+
+  afterEach(async () => {
+    await prismaClient.user.deleteMany({
+      where: {
+        username: "test",
+      },
+    });
+  });
+
+  it("should can register new user", async () => {
+    const result = await supertest(web).post("/api/users").send({
+      username: "test",
+      password: "rahasia123",
+      name: "Verry Kurniawan",
+    });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.username).toBe("test");
+    expect(result.body.data.name).toBe("Verry Kurniawan");
+    expect(result.body.data.password).toBeUndefined();
+  });
+});
