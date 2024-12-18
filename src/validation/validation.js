7c9ce04274b3fc61efbf87f4a7727583d2dc3988
princeofverry <verrykurniawan@students.undip.ac.id>
2024-12-18 07:18:17 +0700

    all test inc (register, nullable, and duplicate data)

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,9 @@
 import { ResponseError } from "../error/response-error.js";
 
 const validate = (schema, request) => {
-  const result = schema.validate(request);
+  const result = schema.validate(request, {
+    abortEarly: false, // cek validasi 1-1 param
+  });
   if (result.error) {
     throw new ResponseError(400, result.error.message);
   } else {
